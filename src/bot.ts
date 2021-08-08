58761494cc192539415d350306c764b039ddce6d
Rafael Oliveira <rafdev.x@gmail.com>
2021-08-08 19:42:57 +0100

    basic skeleton

--- src/bot.ts
+++ src/bot.ts
@@ -1,3 +1,13 @@
-console.log("Hi!");
+import { Client, Intents } from "discord.js";
 
-while (true) {}
+const { DISCORD_TOKEN } = process.env;
+
+const client = new Client({
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.once("ready", () => {
+	console.log("Ready!");
+});
+
+client.login(DISCORD_TOKEN);
