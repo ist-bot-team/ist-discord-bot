8892ef4ce980543e0ed2898d809316db370790c6
Diogo Correia <me@diogotc.com>
2021-10-03 15:47:01 +0100

    Squash prisma migrations for cleaner repository

--- src/prisma/migrations/20211003144607_add_degree_course_features/migration.sql
+++ src/prisma/migrations/20211003144607_add_degree_course_features/migration.sql
@@ -1,26 +1,9 @@
 /*
   Warnings:
 
-  - You are about to drop the `Course` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `Degree` table. If the table is not empty, all the data it contains will be lost.
-  - You are about to drop the `DegreeCourse` table. If the table is not empty, all the data it contains will be lost.
+  - Made the column `role_group_id` on table `role_group_options` required. This step will fail if there are existing NULL values in that column.
 
 */
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Course";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Degree";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "DegreeCourse";
-PRAGMA foreign_keys=on;
-
 -- CreateTable
 CREATE TABLE "degrees" (
     "fenix_id" TEXT NOT NULL PRIMARY KEY,
@@ -54,6 +37,28 @@ CREATE TABLE "degree_courses" (
     CONSTRAINT "degree_courses_degree_fenix_id_fkey" FOREIGN KEY ("degree_fenix_id") REFERENCES "degrees" ("fenix_id") ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT "degree_courses_course_acronym_fkey" FOREIGN KEY ("course_acronym") REFERENCES "courses" ("acronym") ON DELETE CASCADE ON UPDATE CASCADE
 );
+
+-- CreateTable
+CREATE TABLE "course_role_selection_messages" (
+    "injected_role_group_id" TEXT NOT NULL PRIMARY KEY,
+    "message_id" TEXT NOT NULL
+);
+
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_role_group_options" (
+    "label" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "value" TEXT NOT NULL PRIMARY KEY,
+    "emoji" TEXT,
+    "role_group_id" TEXT NOT NULL,
+    CONSTRAINT "role_group_options_role_group_id_fkey" FOREIGN KEY ("role_group_id") REFERENCES "role_groups" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+INSERT INTO "new_role_group_options" ("description", "emoji", "label", "role_group_id", "value") SELECT "description", "emoji", "label", "role_group_id", "value" FROM "role_group_options";
+DROP TABLE "role_group_options";
+ALTER TABLE "new_role_group_options" RENAME TO "role_group_options";
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
 
 -- CreateIndex
 CREATE UNIQUE INDEX "degrees_acronym_key" ON "degrees"("acronym");
