5d0dea84330152fa79cc12dfa7d8ebcd9758c638
Rafael Girão <git@rafael.ovh>
2023-08-24 13:25:08 +0100

    stuff

--- flake.nix
+++ flake.nix
@@ -1,22 +1,59 @@
 {
+  description = "Description for the project";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "flake:nixpkgs";
+      devenv.url = "github:cachix/devenv";
   };
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
-      in {
-        devShell = pkgs.mkShell {
-          buildInputs = with pkgs; [ nodejs-18_x yarn nodePackages.prisma ];
-          shellHook = with pkgs; ''
-            export PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine"
-            export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
-            export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
-            export PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine"
-            export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
-          '';
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+          inputs.devenv.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        # packages.default = pkgs.hello;
+        # packages.default = pkgs.callPackage ./default.nix;
+        packages.default = pkgs.buildNpmPackage rec {
+            pname = "ist-discord-bot";
+            version = "1.0.0";
+            src = ./.;
+            npmDepsHash = "sha256-tuEfyePwlOy2/mOPdXbqJskO6IowvAP4DWg8xSZwbJw=";
+
         };
-      });
+
+        devenv.shells.default = {
+            packages = with pkgs.nodePackages; [
+                pkgs.nodejs_18
+                pkgs.yarn
+                prettier
+                prisma
+            ];
+            env = {
+              PRISMA_MIGRATION_ENGINE_BINARY="${prisma-engines}/bin/migration-engine";
+              PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine";
+              PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node";
+              PRISMA_INTROSPECTION_ENGINE_BINARY="${prisma-engines}/bin/introspection-engine";
+              PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt";
+            };
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
 }
